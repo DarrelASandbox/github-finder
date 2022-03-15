72336ebb0a8acf90fcff3b74ec9bb968b37812a6
DarrelA <darrelaiscoding@gmail.com>
2022-03-15 11:23:57 +0800

    Added 2 components: NavBar & Footer. Added 3 pages: About, Home & NotFound. Setup routes.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-function App() {
-  return (
-    <div className='bg-purple-500'>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Footer, Navbar } from './components/layout/index';
+import { About, Home, NotFound } from './pages/index';
+
+const App = () => (
+  <BrowserRouter>
+    <div className='flex flex-col justify-between h-screen'>
+      <Navbar />
+
+      <main className='container mx-auto px-3 pb-12'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </main>
+
+      <Footer />
     </div>
-  );
-}
+  </BrowserRouter>
+);
 
 export default App;
